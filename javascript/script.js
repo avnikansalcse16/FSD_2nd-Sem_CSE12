bb2488d819e5c2df33a724d98165a9ee80065f7d
Avni Kansal <avnikansal881@gmail.com>
2026-03-13 23:45:41 +0530

    updated files

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+var a=20;
+a="Avni";
+console.log(a);
+a=30;
+console.log(a);
+
+{
+    let name;
+    name="Avni";
+    name="Avni kansal";
+    console.log(name);
+}
+let name="anju";
+console.log(name);
+//const variable
+{
+const name="Avni";
+
+console.log(name);}
